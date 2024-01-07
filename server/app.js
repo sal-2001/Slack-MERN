f995f9b9ff0bb1f8c223a98c51ca2aff6c10d9e3
shijith <shijithsanthosh@gmail.com>
2024-01-07 12:36:38 +0530

    Add apis for creating group chat, renaming it, adding and removin members to groups. Fixed the bug showing res.status is not a function

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use(logger("dev"));
 // app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/chat", chatRoutes);
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,11 +1,11 @@
 const Chat = require("../models/Chat");
 
-const accessChat = async (req, res) => {
+const accessChat = async (req, res, next) => {
   console.log("reached controller");
   const { senderId, recieverId } = req.body;
   if (!recieverId || !senderId) {
     console.log("userId property not present");
-    return res.sendStatus(400);
+    return next(errorHandler(400, "userId property not present"));
   }
 
   let isChat = await Chat.find({
@@ -19,7 +19,7 @@ const accessChat = async (req, res) => {
     .populate("latestMessage");
 
   if (isChat.length > 0) {
-    res.send(isChat[0]);
+    res.json(isChat[0]);
   } else {
     let chatData = {
       chatName: "Sender",
@@ -33,20 +33,20 @@ const accessChat = async (req, res) => {
         "users",
         "-password"
       );
-      res.status(200).send(newChat);
+      res.status(200).json(newChat);
     } catch (err) {
       console.log("something went wrong : ", err);
-      res.status(err.status).send(err.message);
+      next(err);
     }
   }
 };
 
-const fetchAllChats = async (req, res) => {
+const fetchAllChats = async (req, res, next) => {
   console.log("reached controller fetchAllChats");
   const userId = req.query.userId;
   if (!userId) {
     console.log("userId property not present");
-    return res.sendStatus(400);
+    return next(errorHandler(400, "userId property not present"));
   }
 
   try {
@@ -58,11 +58,146 @@ const fetchAllChats = async (req, res) => {
       .populate("latestMessage")
       .sort({ updatedAt: -1 });
 
-    return res.status(200).send(chatList);
+    return res.status(200).json(chatList);
   } catch (err) {
     console.log("something went wrong : ", err.message);
-    return res.status(err.status).send(err.message);
+    next(err);
   }
 };
 
-module.exports = { accessChat, fetchAllChats };
+const createGroupChat = async (req, res, next) => {
+  console.log("reached controller for createGroupChat");
+  const { users, user, groupName } = req.body;
+  if (!users?.length || !groupName) {
+    console.log("All fields are not filled");
+    return next(errorHandler(400, "Please fill all the fields"));
+  }
+
+  if (users?.length < 2) {
+    console.log("less than 2 participants");
+    return next(errorHandler(400, "A group require more than 2 members"));
+  }
+
+  users.push(user);
+
+  try {
+    let groupChat = await Chat.create({
+      chatName: groupName,
+      isGroupChat: true,
+      users: users,
+      groupAdmin: user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    return res.status(200).json(fullGroupChat);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const renameGroup = async (req, res, next) => {
+  const { chatId, chatName } = req.body;
+  if (!chatId || !chatName) {
+    console.log("Chat Id or chat name not present");
+    return next(errorHandler(400, "ChatId or new chatname is missing"));
+  }
+
+  try {
+    let updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        chatName,
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    if (!updatedChat) {
+      return next(errorHandler(404, "Chat group not found"));
+    }
+
+    return res.status(200).json(updatedChat);
+  } catch (err) {
+    console.log("something went wrong : ", err.message);
+    next(err);
+  }
+};
+
+const addToGroup = async (req, res, next) => {
+  const { chatId, userId } = req.body;
+  if (!chatId || !userId) {
+    console.log("Chat Id or chat name not present");
+    return next(errorHandler(400, "ChatId or new chatname is missing"));
+  }
+
+  try {
+    let updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $addToSet: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    if (!updatedChat) {
+      return next(errorHandler(404, "Chat group not found"));
+    }
+
+    return res.status(200).json(updatedChat);
+  } catch (err) {
+    console.log("something went wrong : ", err.message);
+    next(err);
+  }
+};
+
+const removeFromGroup = async (req, res, next) => {
+  console.log("reached controller for removeFromGroup");
+  const { chatId, userId } = req.body;
+  console.log("removing ", userId);
+  if (!chatId || !userId) {
+    console.log("Chat Id or chat name not present");
+    return next(errorHandler(400, "ChatId or new chatname is missing"));
+  }
+
+  try {
+    let updatedChat = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    if (!updatedChat) {
+      return next(errorHandler(404, "Chat group not found"));
+    }
+
+    return res.status(200).json(updatedChat);
+  } catch (err) {
+    console.log("something went wrong : ", err.message);
+    next(err);
+  }
+};
+
+module.exports = {
+  accessChat,
+  fetchAllChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+};
