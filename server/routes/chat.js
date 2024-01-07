fd916a0c9dbc6cbe116d60415c610a2faa1a5f65
Shra1G <shravang49@gmail.com>
2024-01-07 11:03:47 +0530

    modified:   server/routes/chat.js

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 router.post("/", accessChat);
 router.get("/", fetchAllChats);
-router.post("/group", createGroupChat);
-router.put("/rename", renameGroup);
-router.post("/groupremove", removeFromGroup);
-router.post("/groupadd", addToGroup);
+// router.post("/group", createGroupChat);
+// router.put("/rename", renameGroup);
+// router.post("/groupremove", removeFromGroup);
+// router.post("/groupadd", addToGroup);
 
 module.exports = router;
