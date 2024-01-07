9e54121a8b41c755de2b589c7c7f7c1a3195b230
Mohammad Sabeel <mohammad.sk.chy19@itbhu.ac.in>
2024-01-07 17:19:48 +0530

    profile section created

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,11 +1,61 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "../styles/profile.css";
+import useStateValue from "../context/AppContext";
+// import DriveFileRenameOutlineOutlinedIcon from "@mui/icons-material/DriveFileRenameOutlineOutlined";
 function Profile() {
+  const [{ user }, dispatch] = useStateValue();
+  const [edit, setEdit] = useState(false);
+  const handleChange = (e) => {};
   return (
-    <div >
-        <h1>Profile</h1>
+    <div className="profileContainer">
+      <h1 className="profileTitle">Profile</h1>
+      <div className="iamgeContainer">
+        <img src={user.photo} alt="profile pic" className="profilePhoto" />
+        {/* <DriveFileRenameOutlineOutlinedIcon /> */}
+      </div>
+
+      <form className="profileForm">
+        <label for="name" className="profileLabel">Name</label>
+        <input
+          className="profileInput"
+          id="name"
+          type="text"
+          placeholder="Name..."
+          value={user.name}
+          onChange={handleChange}
+        />
+        <label for="email" className="profileLabel">Email</label>
+        <input
+          className="profileInput"
+          id="email"
+          type="email"
+          placeholder="Email..."
+          value={user.email}
+          onChange={handleChange}
+        />
+        <label for="phone" className="profileLabel">Phone</label>
+        <input
+          className="profileInput"
+          id="phone"
+          type=""
+          placeholder="Phone Number..."
+          value={user?.phone}
+          onChange={handleChange}
+        />
+        <label for="password" className="profileLabel">Password</label>
+        <input
+          className="profileInput"
+          id="password"
+          type=""
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button className={`updateButton ${!edit ? "edit" : ""}`}>
+          {edit ? "Update" : "Edit"}
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/topbar.css";
 import useStateValue from '../context/AppContext'
+import Profile from "./Profile";
 function TopBar() {
   const [{user}, dispatch] = useStateValue();
+  const [openProfile, setOpenProfile] = useState(true);
   return <header className="topBar">
     <h2>My Chat</h2>
     <div className="userContainer" >
         <img src={user.photo} className="userImage"/>
     </div>
+    {
+      openProfile && <Profile/>
+    }
   </header>;
 }
 
