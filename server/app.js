eab45c75be962c657ae4a52f2d90ac66fd31fb77
shijith <shijithsanthosh@gmail.com>
2024-01-08 21:37:03 +0530

    Moved socketio event handlers to separate file

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const chatRoutes = require("./routes/chat");
 const messageRoutes = require("./routes/message");
+const socketIOController = require("./controllers/socket");
 
 dotenv.config();
 
@@ -72,19 +73,5 @@ instrument(io, {
 
 io.on("connection", (socket) => {
   console.log("someone connected");
-
-  socket.on("SETUP", (userData) => {
-    console.log("setting up : ", userData?.userId);
-    socket.join(userData?.userId);
-  });
-
-  socket.on("JOIN_ROOM", (roomId) => {
-    console.log("Joining room : ", roomId);
-    socket.join(roomId);
-  });
-
-  socket.on("NEW_MESSAGE_SENT", (message) => {
-    console.log("NEW MESSAGE : ", message);
-    socket.to(message?.chatId).emit("NEW_MESSAGE_RECIEVED", message);
-  });
+  socketIOController(socket);
 });
