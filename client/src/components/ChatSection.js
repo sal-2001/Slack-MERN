32f5129b4c8c58b034aafe7e13c6d41c8db93eff
Shra1G <shravang49@gmail.com>
2024-01-08 17:03:03 +0530

    modified:   client/src/components/ChatSection.js for Chatsection

--- client/src/components/ChatSection.js
+++ client/src/components/ChatSection.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ChatSection() {
   return (
-    <div>ChatSection</div>
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit">Chats</Button>
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit">Groups</Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
   )
 }
